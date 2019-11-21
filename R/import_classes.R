2d4c4a141791cadc2ceb9d3eb7cbb211b2691e01
Russell Hyde <russ.hyde.data@gmail.com>
2019-11-21 17:32:58 +0000

    add GPL annotations during import workfow

--- R/import_classes.R
+++ R/import_classes.R
@@ -97,8 +97,7 @@ methods::setMethod(
   signature = methods::signature(
     config = "MicroarrayImportConfig"
   ),
-  definition = function(
-                        config) {
+  definition = function(config) {
     # Import the microarray data & any phenotypic data from the
     # locally-stored files.
     importer <- config@import_method
@@ -107,7 +106,19 @@ methods::setMethod(
     # Background-Correct the microarray data (& create an ExpressionSet) if
     # necessary
     correcter <- config@normalise_method
-    correcter(initial_marray)
+    normalised_marray <- correcter(initial_marray)
+
+    # Append feature annotations from GPL, if a GPL dataset is available
+    final_marray <- if (
+      !is.na(config@gpl_dir) && !is.na(config@gpl_files)
+    ) {
+      gpl <- import_gpl(config@gpl_files, config@gpl_dir)
+      add_gpl_to_eset(normalised_marray, gpl)
+    } else {
+      normalised_marray
+    }
+
+    final_marray
   }
 )
 
