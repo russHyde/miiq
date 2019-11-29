b276abe4aec1f4a7722f8f7ab1931c883ac1072e
Russ Hyde <russHyde@users.noreply.github.com>
2019-11-29 13:43:44 +0000

    add function 'design_builder' (#22)
    * add function 'design_builder'
    
    This builder-function returns a function that converts an ExpressionSet
    to a design data-frame.
    
    A previously existing function `gld_fnBuilder_exptDesign` returned
    functions that could act on `eset_limma_dataset` or `ExpressionSet`.
    That function has been soft deprecated (it was poorly named and
    complicated to use; it now throws a warning when the returned function
    is used) and has been reimplemented to call `design_builder`.
    
    Tests:
    - the returned function acts on ExpressionSets
    - the returned function fails if it creates a design matrix where the
    row number doesn't match the number of samples in the ExpressionSet.
    
    * move unnecessary checks from gld_fnBuilder... to design_builder

--- R/diffex_functions.R
+++ R/diffex_functions.R
@@ -88,3 +88,69 @@ limma_workflow <- function(
     "fits" = fits
   )
 }
+
+
+###############################################################################
+
+#' Builds a function that makes the design matrix for a dataset
+#'
+#' User passes in the column labels of the phenoData that are to be used in
+#'   making the design
+#' And passes in a design-function that converts these columns into the design
+#' The functionBuilder checks for the validity of the input (treatment_cols /
+#' design_fn) and the built function checks the validity of the ExpressionSet
+#' that gets passed in to it.
+#'
+#' @param        treatment_cols   Which columns of the pData should be kept?
+#' @param        design_fn     Function for making design matrices.
+#'
+#' @importFrom   Biobase       pData   sampleNames
+#'
+#' @include      utils.R   diffex_functions.R
+#' @export
+#'
+
+design_builder <- function(treatment_cols = NULL, design_fn = NULL) {
+  # check type-validity of the treatment_cols and design_fn
+  stopifnot(is.character(treatment_cols) && length(treatment_cols) > 0)
+  stopifnot(is.function(design_fn))
+
+  design_function <- function(gset = NULL) {
+    # check input validity of the Expressionset / geo_limma_dataset
+    if (is.null(gset) || !methods::is(gset, "ExpressionSet")) {
+      stop("'gset' should be an 'ExpressionSet'")
+    }
+
+    # Extract the columns of pData that contain the design-related info
+    # and then construct the design from these columns
+    pdata <- Biobase::pData(gset)
+    if (!all(treatment_cols %in% colnames(pdata))) {
+      missing_cols <- setdiff(treatment_cols, colnames(pdata))
+      stop(paste(
+        "Some treatment_cols were missing from pData:",
+        paste(missing_cols, collapse = "/")
+      ))
+    }
+    treatments <- pdata[, treatment_cols, drop = FALSE]
+    design <- as.data.frame(design_fn(treatments))
+    sn <- Biobase::sampleNames(gset)
+    cn <- make.names(colnames(design))
+
+    if (length(sn) != nrow(design)) {
+      message(
+        c("sampleNames (ExpressionSet): ", paste(sn, collapse = " "))
+      )
+      message(
+        c("sampleNames (design): ", paste(rownames(design), collapse = " "))
+      )
+      print(design)
+      stop("length of sampleNames (in eset) and design do not agree")
+    }
+
+    rownames(design) <- sn
+    colnames(design) <- cn
+    design
+  }
+
+  design_function
+}
