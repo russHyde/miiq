2e4a22c4f0b2cff5565fb28cd38a542119c71256
Russell Hyde <russ.hyde.data@gmail.com>
2019-12-13 16:07:22 +0000

    add testthat to dev workspace

--- random/dev_setup.R
+++ random/dev_setup.R
@@ -2,6 +2,7 @@ message("Setting up Development env for {miiq}")
 
 Sys.setenv(R_REMOTES_UPGRADE = "never")
 library(devtools)
+library(testthat)
 library(goodpractice)
 library(lintr)
 library(styler)
