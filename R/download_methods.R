fefb102dcb9051824b9ca560aa90efb456b51c8e
Russ Hyde <russHyde@users.noreply.github.com>
2019-12-18 15:40:07 +0000

    move download_gpl_annotations function to methods (#31)
    Add tests on download_gpl_annotations and run_download_workflow
    
    These are now written as functions (but were intended to be S4 methods
    on MicroarrayDownloadConfig class). I have no idea why the S4 methods
    could not be dispatched on the MicroarrayDownloadConfig class.
    
    Tests:
    - format of output from download_gpl_annotations & run_download_workflow
    - input validity for download_gpl_annotations & run_download_workflow

--- R/download_methods.R
+++ R/download_methods.R
@@ -1,58 +1,89 @@
 ###############################################################################
 
-# --- Generics
-
-#' Generic for the method 'run_download_workflow'
-#'
-#' @docType      methods
-#' @name         run_download_workflow
-#' @rdname       run_download_workflow-methods
-#'
-#' @export
-#'
-methods::setGeneric(
-  name = "run_download_workflow",
-  def = function(object) standardGeneric("run_download_workflow")
-)
-
-###############################################################################
-
 # --- Methods on "MicroarrayDownloadConfig"
 
 #' 'run_download_workflow' method for the MicroarrayDownloadConfig class.
 #'
-#' @name         run_download_workflow
-#' @rdname       run_download_workflow-methods
-#' @aliases      run_download_workflow,MicroarrayDownloadConfig-method
+#' @param        object        A MicroarrayDownloadConfig object
+#'
+#' @include      download_classes.R
 #'
 #' @export
 #'
-methods::setMethod(
-  f = "run_download_workflow",
-  signature = methods::signature(
-    object = "MicroarrayDownloadConfig"
-  ),
-  definition = function(
-                          object) {
-    downloader <- object@dl_method
 
-    gpl_files <- if (!is.na(object@gpl_acc)) {
-      dl_gpl_annotations(
-        gpl_acc = object@gpl_acc,
-        dest_dir = object@dest_dir,
-        annot_gpl = object@annot_gpl
-      )
-    } else {
-      list()
-    }
+run_download_workflow <- function(object) {
+  # Attempts to write this as a method on MicroarrayDownloadConfig have failed
+  # and I don't know why (hence it is a function, but implemented on MDC)
+  stopifnot(is(object, "MicroarrayDownloadConfig"))
+  downloader <- object@dl_method
 
-    marray_files <- downloader(
-      acc = object@acc,
-      dest_dir = object@dest_dir
-    )
+  gpl_files <- download_gpl_annotations(object)
 
-    append(marray_files, gpl_files)
+  marray_files <- downloader(acc = object@acc, dest_dir = object@dest_dir)
+
+  append(marray_files, gpl_files)
+}
+
+#' Download GPL-annotations from GEO for a MicroarrayDownloadConfig object.
+#'
+#' @param        object        A MicroarrayDownloadConfig object.
+#'
+#' @importFrom   GEOquery      getGEO
+#' @importFrom   RCurl         url.exists
+#'
+#' @include      download_functions.R
+
+download_gpl_annotations <- function(object) {
+  stopifnot(is(object, "MicroarrayDownloadConfig"))
+  # Attempts to write this as a method on MicroarrayDownloadConfig have failed
+  # and I don't know why (hence it is a function, but implemented on MDC)
+
+  # Note that invalid GPL id kills the function.
+  # Note that invalid combination of annot.gpl and gpl.acc kill the function.
+  # Wrapper around getGEO for GPLs that checks that an GPL dataset is
+  #   available before attempting to download it.
+  #
+  # Since it is passed a MicroarrayDownloadConfig
+  # - the gpl_accession will be valid
+  # - the dest_dir will exist
+  # - the annot_gpl will be a scalar logical
+
+  # helpers
+  .gpl_url_exists <- function(x) {
+    gpl_url <- .make_gpl_url(x@gpl_acc, x@annot_gpl)
+    RCurl::url.exists(gpl_url)
   }
-)
+  .make_gpl_filepath <- function(x) {
+    file.path(
+      x@dest_dir, .make_gpl_filename(x@gpl_acc, x@annot_gpl)
+    )
+  }
+  .get_gpl <- function(x) {
+    GEOquery::getGEO(
+      GEO = x@gpl_acc, destdir = x@dest_dir, AnnotGPL = x@annot_gpl
+    )
+  }
+
+  # The filepath into which getGEO would store the GPL dataset
+  gpl_filepath <- .make_gpl_filepath(object)
+
+  if (file.exists(gpl_filepath)) {
+    message("GPL filename ", gpl_filepath, " already exists")
+  } else {
+    if (!.gpl_url_exists(object)) {
+      stop("GPL URL does not exist for GPL id: ", object@gpl_acc)
+    }
+    # Download the GPL dataset for the current microarray platform
+    .get_gpl(object)
+  }
+
+  stopifnot(file.exists(gpl_filepath))
+
+  list(
+    gpl_dir = object@dest_dir,
+    gpl_files = basename(gpl_filepath),
+    annot_gpl = object@annot_gpl
+  )
+}
 
 ###############################################################################
--- tests/testthat/test-download_methods.R
+++ tests/testthat/test-download_methods.R
@@ -4,6 +4,108 @@ context("Tests for Microarray download methods")
 
 ###############################################################################
 
-test_that("Not yet implemented", {})
+mdc_lambda <- function(download_list = list(), dest_dir = tempdir()) {
+  MicroarrayDownloadConfig(
+    acc = "GSE12345",
+    database = "geo",
+    download_method = function(acc, dest_dir) download_list,
+    dest_dir = dest_dir,
+    gpl_acc = "GPL98765",
+    annot_gpl = FALSE
+  )
+}
+
+###############################################################################
+
+test_that("input / output for `run_download_workflow()`", {
+
+  # `run_download_workflow` runs on a MicroarrayDownloadConfig
+
+  expect_error(
+    run_download_workflow("Not a MicroarrayDownloadConfig"),
+    info = "run_download_workflow requires a MicroarrayDownloadConfig"
+  )
+
+  # run_download_workflow returns the microarray-files and the GPL-annotation
+  # files for the platform
+
+  gse_downloads <- list(raw_file = "some_gse_filename")
+  gpl_results <- list(
+    gpl_dir = "some_dir", gpl_files = "some_gpl_filename", annot_gpl = TRUE
+  )
+  mdc <- mdc_lambda(download_list = gse_downloads)
+  mockery::stub(
+    run_download_workflow, "download_gpl_annotations", gpl_results
+  )
+  expect_equal(
+    run_download_workflow(mdc),
+    append(gse_downloads, gpl_results),
+    info = "call to run_download_workflow()"
+  )
+
+})
+
+###############################################################################
+
+test_that("mocked tests for `download_gpl_annotations()`", {
+
+  # the method is only defined for MicroarrayDownloadConfig objects
+
+  expect_error(
+    download_gpl_annotations("Not a MicroarrayDownloadConfig"),
+    info = "download_gpl_annotations requires a MicroarrayDownloadConfig"
+  )
+
+  # expect an error if the GPL URL does not exist
+
+  mdc <- mdc_lambda(dest_dir = tempdir())
+  mockery::stub(download_gpl_annotations, "RCurl::url.exists", FALSE)
+  expect_error(
+    download_gpl_annotations(mdc),
+    info = "URL for the GPL annotations should exist",
+    regexp = "GPL URL does not exist"
+  )
+
+  # if the gpl file already exists, it's path is returned
+
+  td <- tempfile(pattern = "gplTest")
+  dir.create(td)
+  gpl_file <- "GPL98765.tsv"
+  file.create(file.path(td, gpl_file))
+  mdc <- mdc_lambda(dest_dir = td)
+  mockery::stub(download_gpl_annotations, ".make_gpl_filename", gpl_file)
+  expect_equal(
+    download_gpl_annotations(mdc),
+    list(
+      gpl_dir = td, gpl_files = gpl_file, annot_gpl = mdc@annot_gpl
+    ),
+    info = "If a GPL file already exists, it is returned"
+  )
+
+  # download_gpl_annotations should return the directory, and basename for
+  # the downloaded files and whether an annot_gpl was accessed
+
+  td <- tempfile(pattern = "gplTest")
+  dir.create(td)
+  gpl_file <- "GPL98765.tsv"
+  mdc <- mdc_lambda(dest_dir = td)
+  mockery::stub(download_gpl_annotations, "RCurl::url.exists", TRUE)
+  mockery::stub(download_gpl_annotations, ".make_gpl_filename", gpl_file)
+  mockery::stub(
+    download_gpl_annotations, "GEOquery::getGEO", function(...) {
+      file.create(file.path(td, gpl_file))
+    }
+  )
+  expect_equal(
+    download_gpl_annotations(mdc),
+    list(
+      gpl_dir = td, gpl_files = gpl_file, annot_gpl = mdc@annot_gpl
+    ),
+    info = "If the GPL file does not exist locally, it is obtained with getGEO"
+  )
+
+  # expect an error if, after attempting to download the data, the GPL file
+  # does not exist
+})
 
 ###############################################################################
